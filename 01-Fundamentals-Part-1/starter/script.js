52b949095aa1b1575b377d781020dcc460547d48
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-11 16:44:51 +0200

    Completed: 02-9.Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,25 @@
-// let js = "amazing";
-// if (js === "amazing") alert("JavaScript is FUN!");
-
+let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+
+console.log("Tommy");
+console.log(23);
+
+let firstName = "Tommy";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
