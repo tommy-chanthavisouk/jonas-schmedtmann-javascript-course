aedee7a7ae4ba25ec95bfadae49122de5f132384
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-11 17:25:18 +0200

    Completed: 02-11.Assignment

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,12 +1,19 @@
 "use strict";
 
 // Values and Variables
-/*
 let country = "France";
 let continent = "Europe";
 let population = 67;
 
-console.log(country);
-console.log(continent);
-console.log(population);
-*/
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// Data Types
+let isIsland = false;
+let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
