8c1ce28f5e20f42efb01ef6f477a91177f2237b8
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-14 18:35:44 +0200

    Completed: 02-19.Challenge 2

--- 01-Fundamentals-Part-1/starter/challenge.js
+++ 01-Fundamentals-Part-1/starter/challenge.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // CHALLENGE #1
-/*
+// /*
 const massMark = 78;
 const heightMark = 1.69;
 const massJohn = 92;
@@ -15,9 +15,16 @@ const heightJohn = 1.95;
 const BMIMark = massMark / (heightMark * heightMark);
 const BMIJohn = massJohn / (heightJohn * heightJohn);
 
-console.log(BMIMark, BMIJohn);
+// console.log(BMIMark, BMIJohn);
 
 const markHigherBMI = BMIMark > BMIJohn;
 
-console.log(markHigherBMI);
+// console.log(markHigherBMI);
+// */
+
+// CHALLENGE #2
+/*
+if (markHigherBMI)
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+else console.log(`John's BMI ${BMIJohn} is higher than Mark's (${BMIMark})!`);
 */
