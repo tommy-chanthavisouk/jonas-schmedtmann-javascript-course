d771cddc0f910c81ddc10de780a13dc3fef1c045
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-14 15:57:18 +0200

    Completed: 02-14.Operator Precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -100,3 +100,19 @@ const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
 */
+
+// Operator Precedence
+/*
+const now = 2037;
+const ageTommy = now - 1990;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageTommy + ageSarah) / 2;
+console.log(ageTommy, ageSarah, averageAge);
+*/
