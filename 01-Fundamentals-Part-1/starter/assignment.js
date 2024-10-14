d95ce205c89369afe5c0a08955cdf3138e9c36df
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-14 19:03:10 +0200

    Completed: 02-18.Assignment

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -43,3 +43,15 @@ const description2 = `${country} is in ${continent}, and its ${population} milli
 
 // console.log(description1);
 // console.log(description2);
+
+// Taking Decisions: if / else Statements
+/*
+if (population > 33)
+  console.log(
+    `${country}'s population is ${population - 33} million above average`
+  );
+else
+  console.log(
+    `${country}'s population is ${33 - population} million below average`
+  );
+*/
