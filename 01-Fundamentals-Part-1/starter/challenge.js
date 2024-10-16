0eeab1092bfe69935f4becbf4eb1d6f03c7a3aef
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-16 19:42:31 +0200

    Completed: 02-25.Challenge 3

--- 01-Fundamentals-Part-1/starter/challenge.js
+++ 01-Fundamentals-Part-1/starter/challenge.js
@@ -28,3 +28,28 @@ if (markHigherBMI)
   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
 else console.log(`John's BMI ${BMIJohn} is higher than Mark's (${BMIMark})!`);
 */
+
+// CHALLENGE #3
+/*
+// Test data 1
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+
+// Data Bonus 1
+// const scoreDolphins = (97 + 112 + 101) / 3;
+// const scoreKoalas = (109 + 95 + 123) / 3;
+
+// Data Bonus 2
+const scoreDolphins = (97 + 112 + 101) / 3;
+const scoreKoalas = (109 + 95 + 106) / 3;
+
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100)
+  console.log("Dolphins win the trophy");
+else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100)
+  console.log("Koalas win the trophy");
+else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100)
+  console.log("Both win the trophy");
+else console.log("No one wins the trophy");
+*/
