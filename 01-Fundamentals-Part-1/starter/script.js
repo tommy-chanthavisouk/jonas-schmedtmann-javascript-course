70ee759e48a27f37c598400b193a83741183320f
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-16 17:53:21 +0200

    Completed: 02-20.Type Conversion and Coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -166,3 +166,25 @@ if (birthYear <= 2000) {
 
 console.log(century);
 */
+
+// Type Conversion and Coercion
+/*
+// type conversion
+const inputYear = "1990";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Tommy"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" * "2");
+
+let n = "1" + 1; // '11'
+n = n - 1;
+console.log(n);
+*/
