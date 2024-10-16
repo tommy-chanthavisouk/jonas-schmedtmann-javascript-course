d7528ee5e384f1087b5160165ae3062c39e3156c
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-16 20:11:00 +0200

    Completed: 02-26.Assignment

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -85,3 +85,29 @@ if (language === "english" && population < 50 && !isIsland)
   console.log(`You should live in ${country} :)`);
 else console.log(`${country} does not meet your criteria :(`);
 */
+
+// The switch Statement
+/*
+language = "chinese";
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("MOST number of native speakers");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D");
+    break;
+}
+*/
