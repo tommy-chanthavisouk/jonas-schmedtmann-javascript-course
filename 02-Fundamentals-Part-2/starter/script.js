02c716abd6bfd163749320d580ce987896ad6a09
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-16 23:47:01 +0200

    Completed: 03-33.Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -10,3 +10,28 @@ if (hasDriversLicence) console.log("I can drive :D");
 // const interface = "Audio";
 // const private = 534;
 */
+
+// Functions
+/*
+function logger() {
+  console.log("My name is Tommy");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+*/
