f36026e54ef7e6c4ce7d98dd8bc6a21722182a7f
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-16 23:33:42 +0200

    Completed: 03-32.Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+// Activating Strict Mode
+"use strict";
+/*
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest) hasDriversLicence = true;
+if (hasDriversLicence) console.log("I can drive :D");
+
+// const interface = "Audio";
+// const private = 534;
+*/
