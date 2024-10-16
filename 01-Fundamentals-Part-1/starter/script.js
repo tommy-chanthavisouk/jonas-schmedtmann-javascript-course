b31fdb60b9726b17ae277b137cd6e0fe94c9e237
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-16 18:03:30 +0200

    Completed: 02-21.Truthy and Falsy Values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -188,3 +188,21 @@ let n = "1" + 1; // '11'
 n = n - 1;
 console.log(n);
 */
+
+// Truthy and Falsy Values
+/*
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0)); // false
+console.log(Boolean(undefined)); //false
+console.log(Boolean("Tommy")); // true
+console.log(Boolean({})); // true
+console.log(Boolean("")); // false
+
+const money = 100;
+if (money) console.log("Don't spend it all ;)");
+else console.log("You should get a job!");
+
+let height = 0;
+if (height) console.log("YAY! Height is defined");
+else console.log("Height is UNDEFINED");
+*/
