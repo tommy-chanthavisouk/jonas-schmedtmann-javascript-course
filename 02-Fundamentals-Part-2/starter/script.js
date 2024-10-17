5a75139b33b8de3152ea34b9fa1d8c5bac97095e
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-17 18:09:20 +0200

    Completed: 03-34.Functions Declarations vs. Expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -35,3 +35,20 @@ console.log(appleOrangeJuice);
 
 const num = Number("23");
 */
+
+// Functions Declarations vs. Expressions
+/*
+// Function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1990);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1990);
+
+console.log(age1, age2)
+*/
