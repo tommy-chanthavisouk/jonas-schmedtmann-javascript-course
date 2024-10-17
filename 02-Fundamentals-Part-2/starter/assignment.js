300179a2e1857875a5dc9cc760f1dc9c8f06dc31
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-17 18:08:32 +0200

    Completed: 03-33.Assignment

---
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// Functions
+/*
+// France
+const countryFRA = "France";
+let populationFRA = 67;
+const capitalCityFRA = "Paris";
+
+// Belgium
+const countryBEL = "Belgium";
+let populationBEL = 12;
+const capitalCityBEL = "Brussels";
+
+// Germany
+const countryGER = "Germany";
+let populationGER = 85;
+const capitalCityGER = "Berlin";
+
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+
+const descFrance = describeCountry(countryFRA, populationFRA, capitalCityFRA);
+const descBelgium = describeCountry(countryBEL, populationBEL, capitalCityBEL);
+const descGermany = describeCountry(countryGER, populationGER, capitalCityGER);
+
+console.log(descFrance);
+console.log(descBelgium);
+console.log(descGermany);
+*/
