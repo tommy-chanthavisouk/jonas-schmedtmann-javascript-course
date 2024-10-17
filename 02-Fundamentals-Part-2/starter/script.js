4117142207a3dc4f0ab15de5f007c34845ed40b5
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-17 21:05:39 +0200

    Completed: 03-39.Introduction to Arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -109,3 +109,50 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 console.log(yearsUntilRetirement(1990, "Tommy"));
 console.log(yearsUntilRetirement(1950, "Mike"));
 */
+
+//////////////////////////////////////////////////
+// Introduction to Arrays
+/*
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+// friends = ["Bob", "Alice"];
+
+const firstName = "Tommy";
+const tommy = [firstName, "Chanthavisouk", 2037 - 1990, "student", friends];
+console.log(tommy);
+console.log(tommy.length);
+
+// Exercise
+const calcAge = (birthYear) => 2037 - birthYear;
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(ages);
+*/
+
+//////////////////////////////////////////////////
