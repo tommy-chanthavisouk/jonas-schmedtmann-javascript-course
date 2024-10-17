f13d07aaf154f631431832c1bc71e55950209f33
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-17 21:29:08 +0200

    Completed: 03-40.Basic Array Operations (Methods)

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -156,3 +156,42 @@ console.log(ages);
 */
 
 //////////////////////////////////////////////////
+// Basic Array Operations (Methods)
+/*
+const friends = ["Michael", "Steven", "Peter"];
+
+// Add elements
+// push() and unshift() return length of the array
+const newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John");
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last element
+// pop() return the removed element
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First element
+console.log(friends);
+
+// Find index of element
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("Bob")); // -1 if not found
+
+// Find if element in array
+friends.push(23);
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes(23)); // Strict equality
+
+if (friends.includes("Steven")) {
+  console.log(`You have a friend called Steven`);
+}
+*/
+
+//////////////////////////////////////////////////
