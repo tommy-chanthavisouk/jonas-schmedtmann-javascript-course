3e8ba6b4b242885ded2b3b5e9c62cf6b7a3160b6
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-19 14:28:58 +0200

    Completed: 03-46.Assignment

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -158,3 +158,7 @@ console.log(myCountry.population);
 
 myCountry.checkIsland();
 // console.log(myCountry.isIsland);
+
+// Iteration: The for Loop
+// for (let i = 1; i <= 50; i++)
+//   console.log(`Voter number ${i} is currently voting`);
