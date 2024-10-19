e2f8dcc1111ff06e271c72bd4e7b4137c66173ec
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-19 13:13:48 +0200

    Completed: 03-45.Challenge 3

--- 02-Fundamentals-Part-2/starter/challenge.js
+++ 02-Fundamentals-Part-2/starter/challenge.js
@@ -40,3 +40,40 @@ const totals = [
 // console.log(bills);
 // console.log(tips);
 // console.log(totals);
+
+// CHALLENGE 3
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+/*
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`
+  );
+} else if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})`
+  );
+}
+*/
