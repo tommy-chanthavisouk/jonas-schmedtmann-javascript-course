eeff63403ddb67d9453abef4080f242c51c13363
Tommy Chanthavisouk <tommy.chanthavisouk@oclock.school>
2024-10-19 12:16:59 +0200

    Completed: 03-43.Assignment

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -130,3 +130,14 @@ const myCountry = {
   neighbours: neighbours,
 };
 // console.log(myCountry);
+
+// Dot vs. Bracket Notation
+/*
+console.log(
+  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`
+);
+myCountry.population += 2;
+console.log(myCountry.population);
+myCountry["population"] -= 2;
+console.log(myCountry.population);
+*/
